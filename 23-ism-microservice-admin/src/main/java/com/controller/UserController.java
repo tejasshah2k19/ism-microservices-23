f6a2e0d6a2625914bbecbc30e5296ae90f97e2a3
tejas <tejasshah@gmail.com>
2023-05-10 15:36:01 +0530

    adding api gateway only

--- 23-ism-microservice-admin/src/main/java/com/controller/UserController.java
+++ 23-ism-microservice-admin/src/main/java/com/controller/UserController.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.UserEntity;
 import com.repository.UserRepository;
 
 import lombok.extern.slf4j.Slf4j;
-
+@RequestMapping("/admin")
 @RestController
 @Slf4j
 public class UserController {
